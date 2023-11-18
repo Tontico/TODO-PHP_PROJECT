29d0d14a8cb97d1cc4c1a79237d56c88e88604cf
zKittyGB <a-hebert@hotmail.fr>
2023-11-18 01:46:04 +0100

    add the header/head class

---
+++ phpTestComposer/src/views/head.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Keha\Test\views;
+
+//Class that create the Head
+class Head
+{
+    public function displayHead()
+    {
+?>
+
+        <head>
+            <meta charset="UTF-8">
+            <title>Mange tes morts</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+            <style>
+                html,
+                body {
+                    height: 100%;
+                }
+
+                .form-signin {
+                    max-width: 330px;
+                    padding: 1rem;
+                }
+
+                .form-signin .form-floating:focus-within {
+                    z-index: 2;
+                }
+
+                .form-signin input[type="email"] {
+                    margin-bottom: -1px;
+                    border-bottom-right-radius: 0;
+                    border-bottom-left-radius: 0;
+                }
+
+                .form-signin input[type="password"] {
+                    margin-bottom: 10px;
+                    border-top-left-radius: 0;
+                    border-top-right-radius: 0;
+                }
+            </style>
+        </head>
+<?php
+    }
+}
---
+++ phpTestComposer/src/views/header.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Keha\Test\Views;
+
+// Import the urlGenerator Method
+require_once(__DIR__ . './../App/UrlGenerator.php');
+
+
+use Keha\Test\App\UrlGenerator;
+
+
+//Class that create the header
+class Header
+{
+    public function displayHeader()
+    {
+?>
+        <!-- Header with some bootstraps -->
+        <header class="bg-dark p-3 d-flex justify-content-between">
+            <div class="col-1 d-flex text-light align-items-center">
+                <img src="public/assets/logo.png" alt="logo" style="width: 50px; height: 50px;">
+                <p class="m-0 ml-2">Gest'Flex</p>
+            </div>
+            <nav class="navbar navbar-expand-lg navbar-dark col-11">
+                <ul class="navbar-nav mx-auto justify-content-around" style="width:100%">
+                    <li class="nav-item"><a class="nav-link text-light" href="<?= UrlGenerator::generateUrl('IndexController', 'index'); ?>">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="<?= UrlGenerator::generateUrl('SecurityController', 'connexion'); ?>">Se connecter</a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="<?= UrlGenerator::generateUrl('SecurityController', 'inscription'); ?>">S'enregistrer</a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="<?= UrlGenerator::generateUrl('SecurityController', 'dexonnexion'); ?>">Se déconnecter</a></li>
+                </ul>
+            </nav>
+        </header>
+
+
+
+
+<?php
+    }
+}
