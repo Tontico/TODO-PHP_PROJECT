c1ec117c8ea89491db59328fc1ac07de4de24846
Thiblt <t.pouplard@gmx.fr>
2023-11-20 12:49:23 +0100

    view v0.6

--- phpTestComposer/src/Entity/Tache.php
+++ phpTestComposer/src/Entity/Tache.php
@@ -5,8 +5,8 @@ namespace Keha\Test\Entity;
 class Tache
 {
     private int $id_tache;
-    private ?string $nom_tache;
-    private ?string $descritpion_tache;
+    private ?string $Nom_tache;
+    private ?string $Descritpion_tache;
     private ?string $date_debut_tache;
     private ?string $date_realisation_tache;
     private ?string $date_butoire_tache;
@@ -19,8 +19,8 @@ class Tache
     public function __construct($id, $nom, $descritpion, $date_debut, $date_fin, $date_butoire, $idcharge, $idprio, $iduser, $idprojet, $idstatus)
     {
         $this->id_tache = $id;
-        $this->nom_tache = $nom;
-        $this->descritpion_tache = $descritpion;
+        $this->Nom_tache = $nom;
+        $this->Descritpion_tache = $descritpion;
         $this->date_debut_tache = $date_debut;
         $this->date_butoire_tache = $date_butoire;
         $this->date_realisation_tache = $date_fin;
@@ -32,29 +32,29 @@ class Tache
     }
 
     /**
-     * Get the value of nom_tache
+     * Get the value of Nom_tache
      */
     public function getNom_tache()
     {
-        return $this->nom_tache;
+        return $this->Nom_tache;
     }
 
     /**
-     * Get the value of descritpion_tache
+     * Get the value of Descritpion_tache
      */
     public function getDescritpion_tache()
     {
-        return $this->descritpion_tache;
+        return $this->Descritpion_tache;
     }
 
     /**
-     * Set the value of descritpion_tache
+     * Set the value of Descritpion_tache
      *
      * @return  self
      */
-    public function setDescritpion_tache($descritpion_tache)
+    public function setDescritpion_tache($Descritpion_tache)
     {
-        $this->descritpion_tache = $descritpion_tache;
+        $this->Descritpion_tache = $Descritpion_tache;
 
         return $this;
     }
