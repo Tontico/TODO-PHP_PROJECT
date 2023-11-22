5c61dab3d463d0cdc4e01bd51fb075f057b8f383
zKittyGB <a-hebert@hotmail.fr>
2023-11-22 12:58:15 +0100

    fixe css issue

--- phpTestComposer/src/views/forms/InscriptionForm.php
+++ phpTestComposer/src/views/forms/InscriptionForm.php
@@ -63,12 +63,12 @@ class InscriptionForm extends AbstractForm
                 <script>
                     function addShadow() {
                         //Look for the ID loginForm + Add class focused
-                        document.getElementById('form').classList.add('focused');
+                        document.getElementById('security_form').classList.add('focused');
                     }
 
                     function removeShadow() {
                         //Look for the ID loginForm + Remove class focused
-                        document.getElementById('form').classList.remove('focused');
+                        document.getElementById('security_form').classList.remove('focused');
                     }
                 </script>
             </form>
